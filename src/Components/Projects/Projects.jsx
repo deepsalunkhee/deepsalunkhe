c55ecdcd0accdb6be1dabd2b6d6fe335d70c8b87
deepsalunkhee <99858248+deepsalunkhee@users.noreply.github.com>
2022-10-23 14:29:40 +0530

    2.1

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import p1 from '../Projects/enviro.png'
+import p2 from '../Projects/lwyreact.png'
 
 const Projects = () => {
   return (
@@ -11,6 +12,7 @@ const Projects = () => {
             {/* <iframe src="http://environmentaldamagecontrol.me/" title='Project' width={"100%"} height={"600px"} style={{border:"0.3rem solid orange",background:"white"} }  >Environmental-Damage-control</iframe> */}
             
             <a href='http://environmentaldamagecontrol.me/' target="blank"><img src={p1} alt="Project 1" width={"100%"} height={"600px"} style={{border:"0.3rem solid orange",background:"white"}}/></a>
+            <a href='https://deepsalunkhee.github.io/LWY-React-project-/' target="blank"><img src={p2} alt="Project 2" width={"100%"} height={"600px"} style={{border:"0.3rem solid orange",background:"white"}}/></a>
         </div>
       
     </div>
